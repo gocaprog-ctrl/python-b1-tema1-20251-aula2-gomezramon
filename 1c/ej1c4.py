794ddce0fa875ab41fb5f66f5d86a62c9d5c4e82
gocaprog-ctrl <gocaprog@gmail.com>
2025-12-05 17:02:29 +0100

    ej1c4.py
    Implemented a recursive function to check if a word is a palindrome.

--- 1c/ej1c4.py
+++ 1c/ej1c4.py
@@ -41,7 +41,11 @@ Exemple:
 
 
 def is_palindrome(word):
-    # Write here your code
+    if len(word) <= 1:
+        return True
+    if word[0] != word[-1]:
+        return False
+    return is_palindrome(word[1:-1])
     pass
 
 
